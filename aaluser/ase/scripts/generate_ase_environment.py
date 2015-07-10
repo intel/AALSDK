3a344b59f4baa671eb7bbc62252043c70c2cd7b1
Rahul R Sharma <rahul.r.sharma@intel.com>
2015-07-09 17:30:59 -0700

    gen_afu_env.py has symlink problem fixed

--- aaluser/ase/scripts/generate_ase_environment.py
+++ aaluser/ase/scripts/generate_ase_environment.py
@@ -139,7 +139,7 @@ else:
         loc = os.path.abspath(loc)        
         if (os.path.isdir(str(loc))):            
             valid_dirlist.append(loc)
-            str_dirlist = str_dirlist + loc + " "
+            str_dirlist = str_dirlist + loc + "/ "
             print ("\t",loc)
     # If no legal directories, error out
     if len(valid_dirlist) == 0:
@@ -166,7 +166,7 @@ print("")
 print("Finding VHDL files ... ")
 str = ""
 for extn in VHD_EXTENSIONS:
-    str = str + commands_getoutput("find " + str_dirlist + " -type f -name *" + extn)
+    str = str + commands_getoutput("find -L " + str_dirlist + " -type f -name *" + extn)
     if len(str) != 0:
         str = str + "\n\n"
 if len(str.strip()) != 0:
@@ -181,7 +181,7 @@ print("")
 print("Finding {System}Verilog files ... ")
 str = ""
 for extn in VLOG_EXTENSIONS:
-    str = str + commands_getoutput("find " + str_dirlist + " -type f -name *" + extn)
+    str = str + commands_getoutput("find -L " + str_dirlist + " -type f -name *" + extn)
     if len(str) != 0:
         str = str + "\n\n"
 if len(str) != 0:
@@ -194,7 +194,7 @@ else:
 # Recursively find and add directory locations for VH
 print("")
 print("Finding include directories ... ")
-str = commands_getoutput("find " + str_dirlist + " -type d")
+str = commands_getoutput("find -L " + str_dirlist + " -type d")
 str = str.replace("\n", "+")
 if len(str) != 0:
     print("DUT_INCDIR = " + str)
@@ -202,7 +202,7 @@ if len(str) != 0:
 
 # Find ASE HW files ###
 pwd = commands_getoutput("pwd").strip()
-ase_hw_files_str = commands_getoutput ("find " + pwd + "/hw/ -name *.v -or -name *.sv")
+ase_hw_files_str = commands_getoutput ("find -L " + pwd + "/hw/ -name *.v -or -name *.sv")
 if len(ase_hw_files_str) == 0:
     ase_functions.begin_red_fontcolor()
     print("ERROR: ASE files could not be found, please ensure you run this script in the <AALSDK>/ase directory\n")
@@ -218,14 +218,14 @@ fd.write("\n\n")
 # Recursively find and add directory locations for ASE HW files
 print("")
 print("Finding include directories ... ")
-str = commands_getoutput("find " + pwd + "/hw/ -type d")
+str = commands_getoutput("find -L " + pwd + "/hw/ -type d")
 str = str.replace("\n", "+")
 if len(str) != 0:
     print("ASE_INCDIR = " + str)
     fd.write("ASE_INCDIR = " + str + "\n\n")
 
 # Find ASE SW files
-ase_sw_files_str = commands_getoutput ("find " + pwd + "/sw/ -name *.c")
+ase_sw_files_str = commands_getoutput ("find -L " + pwd + "/sw/ -name *.c")
 ase_sw_files_str = ase_sw_files_str.replace("\n", " \\\n\t")
 ase_sw_files_str = ase_sw_files_str[:-1]
 fd.write("ASESW_FILE_LIST = ")
