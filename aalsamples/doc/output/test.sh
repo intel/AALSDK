32a834c554a9963cce03f76810e3f4e6ee67bf7a
Lok Chand Koppaka <lok.chand.koppaka@intel.com>
2015-09-01 15:56:53 -0700

    update doxygen error messages

--- aalsamples/doc/output/test.sh
+++ aalsamples/doc/output/test.sh
@@ -6,11 +6,12 @@ cp doc/output/OUTPUT.dox doc/output/final.dox
 if  [ -a Hello_AAL/SW/helloaal ]; then
    timeout 2s Hello_AAL/SW/helloaal >OUTPUT.txt 2>&1
    temp=`grep -c "Error" OUTPUT.txt`
-   if [ $temp -ne 0 ];
+   wdcnt=`wc -w OUTPUT.txt | awk '{print $1;}'`
+   if [ $temp -ne 0 ] || [ $wdcnt -eq 0 ];
    then
       sed -n 16,26p doc/output/err.txt >>errtemp.txt
       sed -i '/HelloAAL OUTPUT/r errtemp.txt' doc/output/final.dox 
-      rm errtemp.txt
+      rm errtemp.txt OUTPUT.txt
    else
       awk '{print "///"$0}' OUTPUT.txt >OUTPUT1.txt
       sed -i '/HelloAAL OUTPUT/r OUTPUT1.txt' doc/output/final.dox 
@@ -25,12 +26,12 @@ fi
 if  [ -a Hello_CCI_NLB/SW/helloCCInlb ]; then
    timeout 2s Hello_CCI_NLB/SW/helloCCInlb >OUTPUT.txt 2>&1
    temp=`grep -c "Error" OUTPUT.txt`
-   if [ $temp -ne 0 ];
+   wdcnt=`wc -w OUTPUT.txt | awk '{print $1;}'`
+   if [ $temp -ne 0 ] || [ $wdcnt -eq 0 ];
    then
-      temp=0;
       sed -n 29,40p doc/output/err.txt >>errtemp.txt
       sed -i '/HelloCCINLB OUTPUT/r errtemp.txt' doc/output/final.dox 
-      rm errtemp.txt
+      rm errtemp.txt OUTPUT.txt
    else
       awk '{print "///"$0}' OUTPUT.txt >OUTPUT1.txt
       sed -i '/HelloCCINLB OUTPUT/r OUTPUT1.txt' doc/output/final.dox 
@@ -45,11 +46,12 @@ fi
 if  [ -a Hello_SPL_LB/SW/helloSPLlb ]; then
    timeout 2s Hello_SPL_LB/SW/helloSPLlb >OUTPUT.txt 2>&1
    temp=`grep -c "Error" OUTPUT.txt`
-   if [ $temp -ne 0 ];
+   wdcnt=`wc -w OUTPUT.txt | awk '{print $1;}'`
+    if [ $temp -ne 0 ] || [ $wdcnt -eq 0 ];
    then
       sed -n 29,40p doc/output/err.txt >>errtemp.txt
       sed -i '/HelloSPLLB OUTPUT/r errtemp.txt' doc/output/final.dox 
-      rm errtemp.txt
+      rm errtemp.txt OUTPUT.txt
    else
       awk '{print "///"$0}' OUTPUT.txt >OUTPUT1.txt
       sed -i '/HelloSPLLB OUTPUT/r OUTPUT1.txt' doc/output/final.dox 
