b498539beffa44773e68fd584636b84d9beff59f
Lok Chand Koppaka <lok.chand.koppaka@intel.com>
2015-09-01 17:25:03 -0700

    Non-Zero Return update

--- aalsamples/doc/output/test.sh
+++ aalsamples/doc/output/test.sh
@@ -5,9 +5,9 @@ cp doc/output/OUTPUT.dox doc/output/final.dox
 
 if  [ -a Hello_AAL/SW/helloaal ]; then
    timeout 2s Hello_AAL/SW/helloaal >OUTPUT.txt 2>&1
+   status=`echo $?`
    temp=`grep -c "Error" OUTPUT.txt`
-   wdcnt=`wc -w OUTPUT.txt | awk '{print $1;}'`
-   if [ $temp -ne 0 ] || [ $wdcnt -eq 0 ];
+   if [ $temp -ne 0 ] || [ $status -ne 0 ];
    then
       sed -n 16,26p doc/output/err.txt >>errtemp.txt
       sed -i '/HelloAAL OUTPUT/r errtemp.txt' doc/output/final.dox 
@@ -25,9 +25,9 @@ fi
 
 if  [ -a Hello_CCI_NLB/SW/helloCCInlb ]; then
    timeout 2s Hello_CCI_NLB/SW/helloCCInlb >OUTPUT.txt 2>&1
+   status=`echo $?`
    temp=`grep -c "Error" OUTPUT.txt`
-   wdcnt=`wc -w OUTPUT.txt | awk '{print $1;}'`
-   if [ $temp -ne 0 ] || [ $wdcnt -eq 0 ];
+   if [ $temp -ne 0 ] || [ $status -ne 0 ];
    then
       sed -n 29,40p doc/output/err.txt >>errtemp.txt
       sed -i '/HelloCCINLB OUTPUT/r errtemp.txt' doc/output/final.dox 
@@ -45,9 +45,9 @@ fi
 
 if  [ -a Hello_SPL_LB/SW/helloSPLlb ]; then
    timeout 2s Hello_SPL_LB/SW/helloSPLlb >OUTPUT.txt 2>&1
+   status=`echo $?`
    temp=`grep -c "Error" OUTPUT.txt`
-   wdcnt=`wc -w OUTPUT.txt | awk '{print $1;}'`
-    if [ $temp -ne 0 ] || [ $wdcnt -eq 0 ];
+   if [ $temp -ne 0 ] || [ $status -ne 0 ];
    then
       sed -n 29,40p doc/output/err.txt >>errtemp.txt
       sed -i '/HelloSPLLB OUTPUT/r errtemp.txt' doc/output/final.dox 
