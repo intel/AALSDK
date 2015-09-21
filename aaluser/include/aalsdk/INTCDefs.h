12c7d78c7bd72370659f6037a527e5c2ec41fa42
Joseph Grecco <joe.grecco@intel.com>
2015-09-21 04:34:57 -0400

    AIA 4.0 - preparing to test with external app

--- aaluser/include/aalsdk/INTCDefs.h
+++ aaluser/include/aalsdk/INTCDefs.h
@@ -103,6 +103,7 @@
 
 #define  iidAIAService                 __AAL_IID(INTC_sysAIA, 0x0001)   // AIA Service interface
 #define  iidAFUProxy                   __INTC_IID(AAL_sysUAIA, 0x0002)  // AIA Proxy interface
+#define  iidAFUProxyClient             __INTC_IID(AAL_sysUAIA, 0x0003)  // AIA Proxy interface
 
 #define  INTC_sysSampleAFU             INTC_sysBase(0x0005)    // Sample AFU
 
