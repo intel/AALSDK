9b9e45ff93c6f5dda66e62deadf01ed4476c15d5
Joseph Grecco <joe.grecco@intel.com>
2016-01-07 14:52:48 -0500

    Fixed another Windwos build bug where AASService.h was not being generated from substitute

--- aaluser/utils/substitute/substitute.bat
+++ aaluser/utils/substitute/substitute.bat
@@ -1,2 +1,3 @@
 %1 %2\osal\OSALService.h.in %2\osal\OSALService.h @OSAL_SVC_MOD@ libOSAL
+%1 %2\aas\AASService.h.in %2\aas\AASService.h @AAS_SVC_MOD@ libAAS
 %1 %2\aas\AALRuntimeModule.h.in %2\aas\AALRuntimeModule.h @AALRUNTIME_SVC_MOD@ libAAS
