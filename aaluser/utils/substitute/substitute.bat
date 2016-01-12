cb131eb63f9dcc20fb98845033b7fa2b48770bc6
Ananda Ravuri <ananda.ravuri@intel.com>
2016-01-12 11:01:48 -0800

    Sample app windows project files

--- aaluser/utils/substitute/substitute.bat
+++ aaluser/utils/substitute/substitute.bat
@@ -1,3 +1,8 @@
 %1 %2\osal\OSALService.h.in %2\osal\OSALService.h @OSAL_SVC_MOD@ libOSAL
 %1 %2\aas\AASService.h.in %2\aas\AASService.h @AAS_SVC_MOD@ libAAS
 %1 %2\aas\AALRuntimeModule.h.in %2\aas\AALRuntimeModule.h @AALRUNTIME_SVC_MOD@ libAAS
+%1 %2\rm\ResMgrService.h.in %2\rm\ResMgrService.h @RESMGR_SVC_MOD@ libAASResMgr
+%1 %2\rm\RRMService.h.in %2\rm\RRMService.h @RRM_SVC_MOD@ libAASRes
+%1 %2\uaia\AIAService.h.in %2\uaia\AIAService.h @AIASERVICE_SVC_MOD@ libAAService
+%1 %2\service\HWALIAFUService.h.in %2\service\HWALIAFUService.h @HWALIAFU_SVC_MOD@ libHWALIAFU
+%1 %2\service\ALIAFUService.h.in %2\service\ALIAFUService.h @ALIAFU_SVC_MOD@ libALIAFU
