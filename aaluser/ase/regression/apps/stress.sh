39f341d477b85def0cd194c68a4333c6e3bdf9d2
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-02-05 23:55:44 -0800

    Stress test changes

--- aaluser/ase/regression/apps/stress.sh
+++ aaluser/ase/regression/apps/stress.sh
@@ -21,7 +21,6 @@ else
 fi
 
 echo "Stress test will run" $NUM_TESTS "tests"
-
 for i in `seq 1 $NUM_TESTS`;
 do
     if pgrep "ase_simv" -u $USER
@@ -39,7 +38,7 @@ do
 	num_cl=`shuf -i 32-16383 -n 1`
 #	echo $mcl_cnt $num_cl
 	num_cl=$(($num_cl * $mcl_cnt))
-
+	
 	echo ./nlb_test $num_cl $vc_set $mcl_set
 	./nlb_test $num_cl $vc_set $mcl_set > output.log
 	if grep -q "ERROR" output.log
