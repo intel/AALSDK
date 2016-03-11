1063e41dac1679788c105e9463cbf0770d8bc9ab
Sadruta Chandrashekar <sadruta.chandrashekar@intel.com>
2016-03-10 17:01:46 -0800

    MMLink: Added PR hacks

--- aalsamples/MMLink/SW/SigTap.cpp
+++ aalsamples/MMLink/SW/SigTap.cpp
@@ -200,7 +200,7 @@ int SigTapApp::run(mmlink_server *server, char* filename)
 
    ConfigRecord.Add(AAL_FACTORY_CREATE_CONFIGRECORD_FULL_SERVICE_NAME, "libHWALIAFU");
    ConfigRecord.Add(AAL_FACTORY_CREATE_CONFIGRECORD_FULL_AIA_NAME, "libaia");
-   ConfigRecord.Add(keyRegAFU_ID,"C000C966-0D82-4272-9AEF-FE5F84570612"); //LPBK1 AFU ID
+   ConfigRecord.Add(keyRegAFU_ID,"3AB49893-138D-42EB-9642-B06C6B355B87"); //PORT0 AFU ID
 
    Manifest.Add(AAL_FACTORY_CREATE_CONFIGRECORD_INCLUDED, &ConfigRecord);
    Manifest.Add(AAL_FACTORY_CREATE_SERVICENAME, "Signal Tap");
