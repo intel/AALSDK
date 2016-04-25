30242a3dc260d319fd587a35d8d2fb2cf5133283
Tim Whisonant <tim.whisonant@intel.com>
2016-04-25 15:35:40 -0700

    swval: add run-bat.bat, run-nightly.bat, and run-weekly.bat for Windows.

---
+++ swval/win/run-bat.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set errorlevel=
+
+if not exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC" goto vc12
+call aalsdk_build.bat "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC" x64 "Debug" x64 %1
+if '%errorlevel%' == '0' goto runtests
+goto err
+
+:vc12
+call aalsdk_build.bat "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC" x64 "Debug" x64 %1
+if '%errorlevel%' == '0' goto runtests
+goto err
+
+
+:runtests
+cd winbuild\Debug\x64\bin
+.\bat.exe --gtest_print_time=0 --gtest_shuffle
+
+:err
+exit /b %errorlevel%
