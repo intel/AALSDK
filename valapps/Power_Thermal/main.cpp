ea3578bd43c3ca8524fa3b2624676bc8a850a1c3
Tim Whisonant <tim.whisonant@intel.com>
2016-05-09 16:29:31 -0700

    Add valapps project to hold validation tests.

---
+++ valapps/Power_Thermal/main.cpp
@@ -0,0 +1,14 @@
+// INTEL CONFIDENTIAL - For Intel Internal Use Only
+
+// valapps/Power_Thermal/main.cpp
+
+#include <aalsdk/AAL.h>
+#include <aalsdk/Runtime.h>
+
+
+int main(int argc, char *argv[])
+{
+
+   return 0;
+}
+
