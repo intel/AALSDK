3a6449a92914ed264f15d5e9dc9b5f4ba64bf71a
Sadruta Chandrashekar <sadruta.chandrashekar@intel.com>
2016-05-16 17:05:47 -0700

    MMLink valapp: added test SW-STAP-01

--- valapps/Signal_Tap/main.cpp
+++ valapps/Signal_Tap/main.cpp
@@ -1,14 +1,444 @@
-// INTEL CONFIDENTIAL - For Intel Internal Use Only
-
-// valapps/Signal_Tap/main.cpp
-
-#include <aalsdk/AAL.h>
+// Copyright(c) 2007-2016, Intel Corporation
+//
+// Redistribution  and  use  in source  and  binary  forms,  with  or  without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of  source code  must retain the  above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name  of Intel Corporation  nor the names of its contributors
+//   may be used to  endorse or promote  products derived  from this  software
+//   without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO,  THE
+// IMPLIED WARRANTIES OF  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED.  IN NO EVENT  SHALL THE COPYRIGHT OWNER  OR CONTRIBUTORS BE
+// LIABLE  FOR  ANY  DIRECT,  INDIRECT,  INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR
+// CONSEQUENTIAL  DAMAGES  (INCLUDING,  BUT  NOT LIMITED  TO,  PROCUREMENT  OF
+// SUBSTITUTE GOODS OR SERVICES;  LOSS OF USE,  DATA, OR PROFITS;  OR BUSINESS
+// INTERRUPTION)  HOWEVER CAUSED  AND ON ANY THEORY  OF LIABILITY,  WHETHER IN
+// CONTRACT,  STRICT LIABILITY,  OR TORT  (INCLUDING NEGLIGENCE  OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//****************************************************************************
+/// @file HelloALINLB.cpp
+/// @brief Basic ALI AFU interaction.
+/// @ingroup HelloALINLB
+/// @verbatim
+/// Accelerator Abstraction Layer Sample Application
+///
+///    This application is for example purposes only.
+///    It is not intended to represent a model for developing commercially-
+///       deployable applications.
+///    It is designed to show working examples of the AAL programming model and APIs.
+///
+/// AUTHORS: Joseph Grecco, Intel Corporation.
+///
+/// This Sample demonstrates how to use the basic ALI APIs.
+///
+/// This sample is designed to be used with the xyzALIAFU Service.
+///
+/// HISTORY:
+/// WHEN:          WHO:     WHAT:
+/// 12/15/2015     JG       Initial version started based on older sample code.@endverbatim
+//****************************************************************************
+#include <aalsdk/AALTypes.h>
 #include <aalsdk/Runtime.h>
+#include <aalsdk/AALLoggerExtern.h>
+#include <aalsdk/service/IALIAFU.h>
+#include <string.h>
+
+#define MM_DEBUG_LINK_SIGNATURE         0x4170
+#define MM_DEBUG_LINK_VERSION           0x4174
+
+#define EXPECT_SIGNATURE 0x53797343
+#define MAX_SUPPORTED_VERSION 1
+
+using namespace std;
+using namespace AAL;
+
+// Convenience macros for printing messages and errors.
+#ifdef MSG
+# undef MSG
+#endif // MSG
+#define MSG(x) std::cout << __AAL_SHORT_FILE__ << ':' << __LINE__ << ':' << __AAL_FUNC__ << "() : " << x << std::endl
+#ifdef ERR
+# undef ERR
+#endif // ERR
+#define ERR(x) std::cerr << __AAL_SHORT_FILE__ << ':' << __LINE__ << ':' << __AAL_FUNC__ << "() **Error : " << x << std::endl
+
+// Print/don't print the event ID's entered in the event handlers.
+#if 1
+# define EVENT_CASE(x) case x : MSG(#x);
+#else
+# define EVENT_CASE(x) case x :
+#endif
+
+#ifndef CL
+# define CL(x)                     ((x) * 64)
+#endif // CL
+#ifndef LOG2_CL
+# define LOG2_CL                   6
+#endif // LOG2_CL
+#ifndef MB
+# define MB(x)                     ((x) * 1024 * 1024)
+#endif // MB
+#define LPBK1_BUFFER_SIZE        CL(1)
 
 
+/// @addtogroup MMlinkTestApp
+/// @{
+
+
+/// @brief   SW-STAP-01 Obtain the signal tap resource of a port and verify the signature
+///          CSR built into the Signal Tap hardware. This proves access to the HW
+///          and mapping to the user space of the correct location.
+///
+class MMlinkTestApp: public CAASBase, public IRuntimeClient, public IServiceClient
+{
+public:
+
+   MMlinkTestApp();
+   ~MMlinkTestApp();
+
+   btInt run();    ///< Return 0 if success
+
+   // <begin IServiceClient interface>
+   void serviceAllocated(IBase *pServiceBase,
+                         TransactionID const &rTranID);
+
+   void serviceAllocateFailed(const IEvent &rEvent);
+
+   void serviceReleased(const AAL::TransactionID&);
+   void serviceReleaseRequest(IBase *pServiceBase, const IEvent &rEvent);
+   void serviceReleaseFailed(const AAL::IEvent&);
+
+   void serviceEvent(const IEvent &rEvent);
+   // <end IServiceClient interface>
+
+   // <begin IRuntimeClient interface>
+   void runtimeCreateOrGetProxyFailed(IEvent const &rEvent){};    // Not Used
+
+   void runtimeStarted(IRuntime            *pRuntime,
+                       const NamedValueSet &rConfigParms);
+
+   void runtimeStopped(IRuntime *pRuntime);
+
+   void runtimeStartFailed(const IEvent &rEvent);
+
+   void runtimeStopFailed(const IEvent &rEvent);
+
+   void runtimeAllocateServiceFailed( IEvent const &rEvent);
+
+   void runtimeAllocateServiceSucceeded(IBase               *pClient,
+                                        TransactionID const &rTranID);
+
+   void runtimeEvent(const IEvent &rEvent);
+
+   btBool isOK()  {return m_bIsOK;}
+
+   // <end IRuntimeClient interface>
+protected:
+   Runtime        m_Runtime;           ///< AAL Runtime
+   IBase         *m_pAALService;       ///< The generic AAL Service interface for the AFU.
+   IALIMMIO      *m_pALIMMIOService;   ///< Pointer to MMIO Service
+   CSemaphore     m_Sem;               ///< For synchronizing with the AAL runtime.
+   btInt          m_Result;            ///< Returned result value; 0 if success
+
+};
+
+///////////////////////////////////////////////////////////////////////////////
+///
+///  Implementation
+///
+///////////////////////////////////////////////////////////////////////////////
+
+/// @brief   Constructor registers this objects client interfaces and starts
+///          the AAL Runtime. The member m_bisOK is used to indicate an error.
+///
+MMlinkTestApp::MMlinkTestApp() :
+   m_Runtime(this),
+   m_pAALService(NULL),
+   m_pALIMMIOService(NULL),
+   m_Result(0)
+{
+   // Register our Client side interfaces so that the Service can acquire them.
+   //   SetInterface() is inherited from CAASBase
+   SetInterface(iidServiceClient, dynamic_cast<IServiceClient *>(this));
+   SetInterface(iidRuntimeClient, dynamic_cast<IRuntimeClient *>(this));
+
+   // Initialize our internal semaphore
+   m_Sem.Create(0, 1);
+
+   // Start the AAL Runtime, setting any startup options via a NamedValueSet
+
+   // Using Hardware Services requires the Remote Resource Manager Broker Service
+   //  Note that this could also be accomplished by setting the environment variable
+   //   AALRUNTIME_CONFIG_BROKER_SERVICE to librrmbroker
+   NamedValueSet configArgs;
+   NamedValueSet configRecord;
+
+   // Specify that the remote resource manager is to be used.
+   configRecord.Add(AALRUNTIME_CONFIG_BROKER_SERVICE, "librrmbroker");
+   configArgs.Add(AALRUNTIME_CONFIG_RECORD, &configRecord);
+
+   // Start the Runtime and wait for the callback by sitting on the semaphore.
+   //   the runtimeStarted() or runtimeStartFailed() callbacks should set m_bIsOK appropriately.
+   if(!m_Runtime.start(configArgs)){
+	   m_bIsOK = false;
+      return;
+   }
+   m_Sem.Wait();
+   m_bIsOK = true;
+}
+
+/// @brief   Destructor
+///
+MMlinkTestApp::~MMlinkTestApp()
+{
+   m_Sem.Destroy();
+}
+
+/// @brief   run()
+/// Runs the validation test.
+
+btInt MMlinkTestApp::run()
+{
+   cout <<"========================="<<endl;
+   cout <<"= MMLink Validation App ="<<endl;
+   cout <<"========================="<<endl;
+
+   // Request the Servcie we are interested in.
+
+   // NOTE: This example is bypassing the Resource Manager's configuration record lookup
+   //  mechanism.  Since the Resource Manager Implementation is a sample, it is subject to change.
+   //  This example does illustrate the utility of having different implementations of a service all
+   //  readily available and bound at run-time.
+   NamedValueSet Manifest;
+   NamedValueSet ConfigRecord;
+
+   // Service Library to use
+   ConfigRecord.Add(AAL_FACTORY_CREATE_CONFIGRECORD_FULL_SERVICE_NAME, "libALI");
+
+   // the AFUID to be passed to the Resource Manager. It will be used to locate the appropriate device.
+   ConfigRecord.Add(keyRegAFU_ID,"3AB49893-138D-42EB-9642-B06C6B355B87"); //PORT0 AFU ID
+
+   // Add the Config Record to the Manifest describing what we want to allocate
+   Manifest.Add(AAL_FACTORY_CREATE_CONFIGRECORD_INCLUDED, &ConfigRecord);
+
+   // in future, everything could be figured out by just giving the service name
+   Manifest.Add(AAL_FACTORY_CREATE_SERVICENAME, "SigTap Val App");
+
+   MSG("Allocating Service");
+
+   // Allocate the Service and wait for it to complete by sitting on the
+   //   semaphore. The serviceAllocated() callback will be called if successful.
+   //   If allocation fails the serviceAllocateFailed() should set m_bIsOK appropriately.
+   //   (Refer to the serviceAllocated() callback to see how the Service's interfaces
+   //    are collected.)
+   m_Runtime.allocService(dynamic_cast<IBase *>(this), Manifest);
+   m_Sem.Wait();
+   if(!m_bIsOK){
+      ERR("Allocation failed\n");
+      goto done_0;
+   }
+
+   if(true == m_bIsOK){
+
+      btUnsigned32bitInt signature, version, wrCapacity;
+
+      m_pALIMMIOService->mmioRead32(MM_DEBUG_LINK_SIGNATURE, &signature);
+      if ( signature != EXPECT_SIGNATURE)
+      {
+         ERR( "Unverified Signature\n");
+         m_Result++;
+      }
+
+      m_pALIMMIOService->mmioRead32(MM_DEBUG_LINK_VERSION, &version);
+      if ( version > MAX_SUPPORTED_VERSION)
+      {
+         ERR( "Unsupported Version\n");
+         m_Result++;
+      }
+
+   }
+   MSG("Done Running Test");
+
+   // Clean-up and return
+done_1:
+   // Freed all three so now Release() the Service through the Services IAALService::Release() method
+   (dynamic_ptr<IAALService>(iidService, m_pAALService))->Release(TransactionID());
+   m_Sem.Wait();
+
+done_0:
+   m_Runtime.stop();
+   m_Sem.Wait();
+
+   return m_Result;
+}
+
+//=================
+//  IServiceClient
+//=================
+
+// <begin IServiceClient interface>
+void MMlinkTestApp::serviceAllocated(IBase *pServiceBase,
+                                      TransactionID const &rTranID)
+{
+   // Save the IBase for the Service. Through it we can get any other
+   //  interface implemented by the Service
+   m_pAALService = pServiceBase;
+   ASSERT(NULL != m_pAALService);
+   if ( NULL == m_pAALService ) {
+      m_bIsOK = false;
+      return;
+   }
+
+   // Documentation says HWALIAFU Service publishes
+   //    IALIMMIO as subclass interface. Used to set/get MMIO Region
+   m_pALIMMIOService = dynamic_ptr<IALIMMIO>(iidALI_MMIO_Service, pServiceBase);
+   ASSERT(NULL != m_pALIMMIOService);
+   if ( NULL == m_pALIMMIOService ) {
+      m_bIsOK = false;
+      return;
+   }
+
+   MSG("Service Allocated");
+   m_Sem.Post(1);
+}
+
+void MMlinkTestApp::serviceAllocateFailed(const IEvent &rEvent)
+{
+   ERR("Failed to allocate Service");
+    PrintExceptionDescription(rEvent);
+   ++m_Result;                     // Remember the error
+   m_bIsOK = false;
+
+   m_Sem.Post(1);
+}
+
+ void MMlinkTestApp::serviceReleased(TransactionID const &rTranID)
+{
+    MSG("Service Released");
+   // Unblock Main()
+   m_Sem.Post(1);
+}
+
+ void MMlinkTestApp::serviceReleaseRequest(IBase *pServiceBase, const IEvent &rEvent)
+ {
+    MSG("Service unexpected requested back");
+    if(NULL != m_pAALService){
+       IAALService *pIAALService = dynamic_ptr<IAALService>(iidService, m_pAALService);
+       ASSERT(pIAALService);
+       pIAALService->Release(TransactionID());
+    }
+ }
+
+
+ void MMlinkTestApp::serviceReleaseFailed(const IEvent        &rEvent)
+ {
+    ERR("Failed to release a Service");
+    PrintExceptionDescription(rEvent);
+    m_bIsOK = false;
+    m_Sem.Post(1);
+ }
+
+
+ void MMlinkTestApp::serviceEvent(const IEvent &rEvent)
+{
+   ERR("unexpected event 0x" << hex << rEvent.SubClassID());
+   // The state machine may or may not stop here. It depends upon what happened.
+   // A fatal error implies no more messages and so none of the other Post()
+   //    will wake up.
+   // OTOH, a notification message will simply print and continue.
+}
+// <end IServiceClient interface>
+
+
+ //=================
+ //  IRuntimeClient
+ //=================
+
+  // <begin IRuntimeClient interface>
+ // Because this simple example has one object implementing both IRuntieCLient and IServiceClient
+ //   some of these interfaces are redundant. We use the IServiceClient in such cases and ignore
+ //   the RuntimeClient equivalent e.g.,. runtimeAllocateServiceSucceeded()
+
+ void MMlinkTestApp::runtimeStarted( IRuntime            *pRuntime,
+                                      const NamedValueSet &rConfigParms)
+ {
+    m_bIsOK = true;
+    m_Sem.Post(1);
+ }
+
+ void MMlinkTestApp::runtimeStopped(IRuntime *pRuntime)
+  {
+     MSG("Runtime stopped");
+     m_bIsOK = false;
+     m_Sem.Post(1);
+  }
+
+ void MMlinkTestApp::runtimeStartFailed(const IEvent &rEvent)
+ {
+    ERR("Runtime start failed");
+    PrintExceptionDescription(rEvent);
+ }
+
+ void MMlinkTestApp::runtimeStopFailed(const IEvent &rEvent)
+ {
+     MSG("Runtime stop failed");
+     m_bIsOK = false;
+     m_Sem.Post(1);
+ }
+
+ void MMlinkTestApp::runtimeAllocateServiceFailed( IEvent const &rEvent)
+ {
+    ERR("Runtime AllocateService failed");
+    PrintExceptionDescription(rEvent);
+ }
+
+ void MMlinkTestApp::runtimeAllocateServiceSucceeded(IBase *pClient,
+                                                     TransactionID const &rTranID)
+ {
+     MSG("Runtime Allocate Service Succeeded");
+ }
+
+ void MMlinkTestApp::runtimeEvent(const IEvent &rEvent)
+ {
+     MSG("Generic message handler (runtime)");
+ }
+ // <begin IRuntimeClient interface>
+
+/// @} group MMlinkTestApp
+
+
+//=============================================================================
+// Name: main
+// Description: Entry point to the application
+// Inputs: none
+// Outputs: none
+// Comments: Main initializes the system. The rest of the example is implemented
+//           in the object theApp.
+//=============================================================================
 int main(int argc, char *argv[])
 {
+   MMlinkTestApp theApp;
+   if(!theApp.isOK()){
+      ERR("Runtime Failed to Start");
+      exit(1);
+   }
+   btInt Result = theApp.run();
 
-   return 0;
+   if(0 == Result){
+      MSG ("TEST PASS!");
+   }else{
+      ERR ("TEST FAIL!");
+   }
+
+   MSG("Done");
+   return Result;
 }
 
